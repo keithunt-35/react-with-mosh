fb65a76ba97db31ac7efea6307260bab5b5ba073
keithunt-35 <keithunt35@duck.com>
2024-12-08 16:55:55 +0300

    independent state management

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import ListGroup from "./components/ListGroup";
 function App(){
   const name = 'keithunt';
  
-  return <div><ListGroup/></div>;
+  return <div><ListGroup/><ListGroup/></div>;
 
 }
 export default App;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import { Fragment } from "react/jsx-runtime";
-import { MouseEvent } from "react";
+
 
 function ListGroup() {
   let item = ["Kampala", "Nairobi", "Kisumu", "Jinja", "Hoima"];
+  //let selectedIndex = 0;
+  //hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  //const [name, setName ] = useState('');
 
-  //eventhandler
-  const handleClick = (event: MouseEvent) => console.log(event);
+ 
+  
   return (
     <Fragment>
       <h1>Listing</h1>
@@ -13,9 +18,9 @@ function ListGroup() {
       <ul className="list-group">
         {item.map((item, index) => (
           <li
-            className="list-group-item"
+            className={selectedIndex === index? 'list-group-item active':'list-group-item'}
             key={item}
-            onClick={handleClick}
+            onClick={() => {setSelectedIndex(index);}}
           >
             {item}
           </li>
