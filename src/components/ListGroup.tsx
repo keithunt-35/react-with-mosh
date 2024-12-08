261cfc6d08a04317fe7f799062b3538109fc260f
keithunt-35 <keithunt35@duck.com>
2024-12-08 16:32:37 +0300

    passing reference of an event handler

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,18 +1,21 @@
 import { Fragment } from "react/jsx-runtime";
+import { MouseEvent } from "react";
 
 function ListGroup() {
   let item = ["Kampala", "Nairobi", "Kisumu", "Jinja", "Hoima"];
 
+  //eventhandler
+  const handleClick = (event: MouseEvent) => console.log(event);
   return (
     <Fragment>
       <h1>Listing</h1>
       {item.length === 0 && <p>message not found</p>}
       <ul className="list-group">
-        {item.map((item) => (
+        {item.map((item, index) => (
           <li
             className="list-group-item"
             key={item}
-            onClick={() => console.log(item)}
+            onClick={handleClick}
           >
             {item}
           </li>
